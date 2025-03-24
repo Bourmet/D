b3588531f8113201160c1b299df853b931fe285b
JReis <jedor.reis@gmail.com>
2025-03-24 15:48:52 -0300

    first commit

---
+++ src/main/java/com/D/DolceForno/DolceFornoApplication.java
@@ -0,0 +1,13 @@
+package com.D.DolceForno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolceFornoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolceFornoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/D/DolceForno/DolceFornoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.D.DolceForno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolceFornoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
